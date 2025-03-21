a1800d63b2424b83dc5f8f079a1412094fa9416a
Feyishesan <39010441+Feyishesan@users.noreply.github.com>
2025-03-21 16:13:50 -0400

    Update GreetingResource.java

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -13,15 +13,15 @@ public class GreetingResource {
     GreetingService service;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_HTML)
     @Path("/greeting/{name}")
     public String greeting(String name) {
-        return service.greeting(name);
+        return <h1>service.greeting(name) + </h1>;
     }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "<h1>hello</h1>";
     }
 }
