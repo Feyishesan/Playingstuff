89c744f4c657dace4b70bec6a5dabc3cc53e8b22
Feyishesan <39010441+Feyishesan@users.noreply.github.com>
2025-03-31 14:05:06 -0400

    Update GreetingResource.java

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -6,7 +6,6 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 @Path("/hello")
 public class GreetingResource {
@@ -17,7 +16,7 @@ public class GreetingResource {
   @GET
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greeting/{name}")
-    public String greeting(@PathParam String name) {
+    public String greeting(String name) {
         return service.greeting(name);
     }
 
