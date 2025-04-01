4326b20357f5cb520cb8ae7396338c985440d292
Feyishesan <sesanojelade@gmail.com>
2025-04-01 15:34:52 +0000

    HTML fonts

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -6,25 +6,22 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-
 @Path("/hello")
 public class GreetingResource {
 
     @Inject
     GreetingService service;
 
- @GET
+    @GET
     @Produces(MediaType.TEXT_HTML)
     @Path("/greeting/{name}")
     public String greeting(String name) {
-        return "<h>" + service.greeting(name) + "</h>";
+        return "<h1>" + service.greeting(name) + "</h1>";
     }
 
     @GET
     @Produces(MediaType.TEXT_HTML)
     public String hello() {
-        return "<h>hello</h>";
+        return "<h1>hello</h1>";
     }
 }
-
-
