df631b7249be09b21919739d8c0b529044dc6e33
Feyishesan <sesanojelade@gmail.com>
2025-04-02 18:29:46 +0000

    Big fonts

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -11,17 +11,16 @@ public class GreetingResource {
 
     @Inject
     GreetingService service;
-
-      @GET
-    @Produces(MediaType.TEXT_PLAIN)
+ @GET
+    @Produces(MediaType.TEXT_HTML)
     @Path("/greeting/{name}")
     public String greeting(String name) {
-        return service.greeting(name);
+        return "<h1>" + service.greeting(name) + "</h1>";
     }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.TEXT_HTML)
     public String hello() {
-        return "hello";
+        return "<h1>hello</h1>";
     }
 }
